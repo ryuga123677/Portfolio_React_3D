d49b590eba9919095c9ae0ca4c72eea6476e0ccc
ryuga123677 <kishanss311@gmail.com>
2024-09-19 02:33:57 +0530

    initial commit

---
+++ portfolio-final/src/App.jsx
@@ -0,0 +1,14 @@
+
+import Portfolio from "./components/portfolio";
+
+function App() {
+  return (
+    <>
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <Portfolio />
+      </div>
+    </>
+  );
+}
+
+export default App;
